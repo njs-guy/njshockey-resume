ec64fa0d99eda02ab82a04a2191d2240968b1985
Nick Shockey <shockeynj@gmail.com>
2022-09-16 13:09:35 -0400

    Panels can now optionally have titles

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,10 +1,14 @@
 interface PanelProps {
+    heading?: string,
     text: string,
 }
 
-const Panel = ({text}: PanelProps) => {
+const Panel = ({heading = '', text}: PanelProps) => {
     return (
         <div className="panel">
+            {heading !== '' &&
+                <h1 className="prose-2xl font-bold">{heading}</h1>
+            }
             <p>{text}</p>
         </div>
     )
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,6 +8,10 @@ const Home = () => {
                 <Panel text="
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non enim lacus. Morbi at vestibulum odio. Ut scelerisque magna sed mauris feugiat, et ornare sapien mollis. Nunc a tellus in odio tempus rhoncus. Curabitur sed neque quis orci facilisis condimentum et sed magna. Phasellus posuere tempus imperdiet. Nulla facilisi.
                 " />
+                <Panel heading="The facts"
+                text="
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non enim lacus. Morbi at vestibulum odio. Ut scelerisque magna sed mauris feugiat, et ornare sapien mollis. Nunc a tellus in odio tempus rhoncus. Curabitur sed neque quis orci facilisis condimentum et sed magna. Phasellus posuere tempus imperdiet. Nulla facilisi.
+                " />
                 <button type="button" className="btn btn-primary">Wow</button>
                 <div className="panel">
                     <IconGrid />
