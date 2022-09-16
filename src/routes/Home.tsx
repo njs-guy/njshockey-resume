7247c29012ce5c76cb9fab57dc700919177fd754
Nick Shockey <shockeynj@gmail.com>
2022-09-16 13:15:27 -0400

    Put info from old site into the home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,13 +6,18 @@ const Home = () => {
         <span>
             <div className="panelContainer">
                 <Panel text="
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non enim lacus. Morbi at vestibulum odio. Ut scelerisque magna sed mauris feugiat, et ornare sapien mollis. Nunc a tellus in odio tempus rhoncus. Curabitur sed neque quis orci facilisis condimentum et sed magna. Phasellus posuere tempus imperdiet. Nulla facilisi.
+                Hello! I'm Nick Shockey. I am a recent and driven graduate looking to gain professional experience with multiple open-source projects under my belt. 
+                With about a 3.7 GPA, I was able to make the Dean’s List for most semesters and achieved Magnum Cum Laude. 
+                I am a hard worker that is currently looking for work in IT, programming, or software development. 
+                This e-portfolio will tell you about my professional goals, skills, and experience.
                 " />
-                <Panel heading="The facts"
+                <Panel heading="Professional Goals"
                 text="
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non enim lacus. Morbi at vestibulum odio. Ut scelerisque magna sed mauris feugiat, et ornare sapien mollis. Nunc a tellus in odio tempus rhoncus. Curabitur sed neque quis orci facilisis condimentum et sed magna. Phasellus posuere tempus imperdiet. Nulla facilisi.
+                My ultimate goal is to become a software developer. 
+                However, I felt that I didn't know enough after graduation to achieve this goal. 
+                So, I went online and learned more skills such as Python, HTML, CSS, JavaScript, and Git. 
+                From there, I made a few open source projects to demonstrate those skills and tried to practice just about every day.
                 " />
-                <button type="button" className="btn btn-primary">Wow</button>
                 <div className="panel">
                     <IconGrid />
                 </div>
