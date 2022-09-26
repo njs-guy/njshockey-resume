202bbedb901247ab95755df7dbf2ac724aa03923
Nick Shockey <shockeynj@gmail.com>
2022-09-26 14:57:14 -0400

    Added open cover letter and hard skills to resume

--- src/components/MarkdownText.tsx
+++ src/components/MarkdownText.tsx
@@ -9,8 +9,16 @@ const MarkdownText = ({text}: MdTxtProps) => {
         return <h1 className="panelHeading">{props.children}</h1>;
     }
 
+    function h2HTML(props: any) {
+        return <h2 className="prose-xl font-bold">{props.children}</h2>
+    }
+
     function pHTML(props: any) {
-        return <p className="panelHeading">{props.children}</p>;
+        return <p>{props.children}</p>;
+    }
+
+    function ulHTML(props: any) {
+        return <ul className="list-disc list-inside">{props.children}</ul>
     }
 
     return(
@@ -18,6 +26,9 @@ const MarkdownText = ({text}: MdTxtProps) => {
             
             components={{ 
                 h1: headingHTML,
+                h2: h2HTML,
+                ul: ulHTML,
+                p: pHTML,
             }}
             
             children={text} />
