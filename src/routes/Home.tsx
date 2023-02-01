31db589a75a3d3af4d3abcb8c514938b3db4b60e
Nick Shockey <shockeynj@gmail.com>
2023-02-01 14:58:49 -0500

    Fixed greeting text

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -10,6 +10,13 @@ const Home = () => {
 		updateIcons();
 	});
 
+	const greetingText =
+		"Hello! I'm Nick Shockey. " +
+		"I am a recent and driven graduate looking to gain professional experience with multiple open-source projects under my belt. " +
+		"With about a 3.7 GPA, I was able to make the Dean's List for most semesters and achieved Magnum Cum Laude. " +
+		"I am a hard worker that is currently looking for work in IT, programming, or software development. " +
+		"This e-portfolio will tell you about my professional goals, skills, and experience.";
+
 	const proGoalsMd =
 		"My ultimate goal is to become a software developer. " +
 		"However, I felt that I didn't know enough after graduation to achieve this goal. " +
@@ -21,12 +28,7 @@ const Home = () => {
 			<div className="panelContainer">
 				<Panel
 					img="https://via.placeholder.com/160"
-					text="
-                Hello! I'm Nick Shockey. I am a recent and driven graduate looking to gain professional experience with multiple open-source projects under my belt. 
-                With about a 3.7 GPA, I was able to make the Dean’s List for most semesters and achieved Magnum Cum Laude. 
-                I am a hard worker that is currently looking for work in IT, programming, or software development. 
-                This e-portfolio will tell you about my professional goals, skills, and experience.
-                "
+					text={greetingText}
 				/>
 				<Panel
 					heading="Professional Goals"
